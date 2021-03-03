bc4060a7c50c67af5ac0f2cad5b0cf81c86bf553
Yannick <wgyannick@gmail.com>
2021-03-03 14:01:18 +0100

    Added back arrow for CrypocurrencyDetails Activity.

--- app/src/main/java/com/example/cours/list/cryptocurrency/CryptocurrencyDetails.kt
+++ app/src/main/java/com/example/cours/list/cryptocurrency/CryptocurrencyDetails.kt
@@ -1,7 +1,9 @@
 package com.example.cours.list.cryptocurrency
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cours.R
 
 class CryptocurrencyDetails : AppCompatActivity() {
@@ -18,4 +20,18 @@ class CryptocurrencyDetails : AppCompatActivity() {
         //set back button
         actionbar.setDisplayHomeAsUpEnabled(true)
     }
+   override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return true
+    }
+
 }
