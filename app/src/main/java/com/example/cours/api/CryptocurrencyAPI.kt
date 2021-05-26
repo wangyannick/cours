03a904f952283fd6eeb43be288d73be985afcc81
Yannick <wgyannick@gmail.com>
2021-05-26 10:53:22 +0200

    Imported new library to create a Observer for call api X seconds.

--- app/src/main/java/com/example/cours/api/CryptocurrencyAPI.kt
+++ app/src/main/java/com/example/cours/api/CryptocurrencyAPI.kt
@@ -3,6 +3,7 @@ package com.example.cours.api
 import retrofit2.http.GET
 import retrofit2.Call
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CryptocurrencyAPI {
     @GET("tickers/?quotes=EUR")
@@ -10,4 +11,12 @@ interface CryptocurrencyAPI {
 
     @GET("coins/{coins}")
     fun getCurrency(@Path("coins") coins: String): Call<CryptocurrencyResponse>
+
+    @GET("coins/{coins}/ohlcv/historical")
+    fun getCryptocurrencyOHLCV(
+        @Path("coins") coins: String,
+        @Query("start") start: String,
+        @Query("end") end: String,
+        @Query("quote") quote: String
+    ): Call<ArrayList<CryptocurrencyOHLCV>>
 }
